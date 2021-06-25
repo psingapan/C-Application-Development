fd102996c94a9f1d2255792c980678c30d6a5e4f
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-06-24 21:53:06 -0700

    Add files via upload

---
+++ Singapan_CSC205_Week6_HW1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Singapan_CSC205_Week6_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reading();
+          
+
+            static void reading()
+            {
+                //Assigning the file name to the variable.
+                var path = "Mockingjay.txt";
+                var file = "NewLine" + path;
+
+                
+
+                //Activate methods from StreamReader and StreamWriter while referencing the file.
+                using var sr = new StreamReader(path);
+                using var sw = new StreamWriter(file);
+
+
+                string line = "";
+                int counter = 0;
+
+
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(counter + ":" + line);
+                    sw.WriteLine(counter + ":" + line);
+                    counter++;
+                }
+            }
+
+           
+
+        }
+    }
+}
+
+       
+
