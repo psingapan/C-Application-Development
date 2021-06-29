038541914e99a4b7165ff3a0a77456cc25ee667a
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-06-29 13:36:06 -0700

    Add files via upload
    This exercise is based on the existing code (Line 1 - 62) from the course github link below:
    https://github.com/dsfiles/CSC205/blob/main/week07/homework-code
    Complete the following steps:
      1) Test run the program.
      2) Summarize the whole program and explain the following lines (github file lines): 11, 40 and 55.
      3) Add a Rectangle class that inherits Shape, and test it in Program.cs.
      4) Extra credit: add a Triangle class and test it in Program.cs.

---
+++ Singapan_CSC205_Week7HW1.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * CSC205 Week7 Programming Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Shape class: Shape.cs
+ */
+
+using System;
+
+public abstract class Shape
+{
+    public abstract double Area(); //this is a method
+    public static double GetArea(Shape shape)
+    {
+        return shape.Area();
+    }
+}
+
+public class Square : Shape //inheritance
+{
+    private double size;
+    public Square(double length)
+    {
+        this.size = length;
+    }
+    public override double Area()
+    {
+        return Math.Pow(size, 2);
+    }
+}
+
+public class Circle : Shape
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public override double Area()
+    {
+        return Math.Round(Math.PI * Math.Pow(radius, 2), 2); //This is the Area() method within the Circle class invoked through iteration to return the circle area given a radius of 10.
+    }
+}
+
+//Add a rectangle class that inherits shape, and test it in program.cs
+public class Rectangle: Shape
+{
+    private double size;
+
+    public Rectangle(double length)
+    {
+        this.size = length;
+    }
+    public override double Area()
+    {
+        return Math.Pow(size, 2);
+    }
+}
+
+//Extra credit: add a Triangle class and test it in Program.cs.
+public class Triangle : Shape
+{
+    private double b;
+    private double h;
+    public Triangle (double b , double h)
+    {
+        this.b = b;
+        this.h = h;
+    }
+    public override double Area()
+    {
+        return 0.5 * (b * h);
+    }
+}
+
+
+/*
+ * CSC205 Week7 Programming Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Tester: Program.cs
+ */
+
+public class Program
+{
+    public static void Main()
+    {
+        Shape[] shapes = { new Square(10), new Circle(10), new Rectangle(10), new Triangle(10,10) }; // creating a new shape array to hold the radius and length of the circle and square objects.
+                                                            
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Area of {shape}: {shape.Area()}");
+            Console.WriteLine($"Area of {shape}: {Shape.GetArea(shape)}, again");
+        }
+    }
+}
+
+// 2) Summarize the whole program and explain the following lines (github file lines): 11, 40 and 55.
+
+//The program displays the area of the shapes stored in the array after being passed the values. The console displays the area twice ( to exemplify the result called from two different methods) for each shape. 
+//The results are stored in the (shapes) array.
